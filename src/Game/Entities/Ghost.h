d279c60e2296afa6714293c8452a96cd4e4d8666
Johncarlo <johncarlo.perez.talavera@gmail.com>
2020-10-23 17:09:03 -0700

    moving code

--- src/Game/Entities/Ghost.h
+++ src/Game/Entities/Ghost.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "Entity.h"
 #include "EntityManager.h"
+
+enum DIRECTION {
+    U,
+    D,
+    L,
+    R
+};
+
 enum color{
         RED = 1, 
         PINK = 2,
@@ -11,10 +19,17 @@ class Ghost: public Entity{
     private:
         color ghostColor;
         int speed = 4;
+        bool canMove;
+        EntityManager *em;
+        DIRECTION position = U;
     public:
         Ghost(int, int, int, int, ofImage);
         Ghost(int, int, int, int, ofImage, color); //overloaded to accept a color indicator and entity manager
+        Ghost(int, int, int, int, ofImage,EntityManager*);
         int getSpeed(){ return this->speed;}
         void setSpeed(int newSpeed){ this->speed = newSpeed;}
         color getColor(){ return ghostColor; }
+        
+        void tick();
+        void checkCollisions();
 };
